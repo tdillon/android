5016c82c00992a0c1561dfd9ab22a359bb2ddae7
tdillon <tdillon@users.noreply.github.com>
2016-07-10 15:57:36 -0400

    init project

---
+++ app/src/main/java/tgd/mindless/drone/calculatornumberone/MainActivity.java
@@ -0,0 +1,13 @@
+package tgd.mindless.drone.calculatornumberone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

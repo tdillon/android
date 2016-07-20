e315792468d0b59707098f9118ebe8efdf7a983b
tdillon <tdillon@users.noreply.github.com>
2016-07-19 21:02:02 -0400

    trying conditional tag based builds/deploys

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo $TRAVIS_TAG
 
 ./gradlew assembleRelease
 
-if [ "${TRAVIS_TAG}" = "production" ]; then
+if [ "${TRAVIS_TAG}" != "" ]; then
   jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks app/build/outputs/apk/app-release-unsigned.apk fastlanetraviscipoc
   "/usr/local/android-sdk/build-tools/23.0.3/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk"
   ls -la app/build/outputs/apk

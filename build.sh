17fb390a7e5ca211ba136162d51c6ab6796513b9
tdillon <tdillon@users.noreply.github.com>
2016-07-19 21:19:46 -0400

    trying conditional tag based builds/deploys

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo $TRAVIS_TAG
 
 if [ "${TRAVIS_TAG}" != "" ]; then
   jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks app/build/outputs/apk/app-release-unsigned.apk fastlanetraviscipoc
-  "/usr/local/android-sdk/build-tools/23.0.3/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk"
+  /usr/local/android-sdk/build-tools/23.0.3/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk app/build/outputs/apk/app-release.apk
   ls -la app/build/outputs/apk
   supply -v
   supply run -j gpdpapi.service_account.json -p tgd.mindless.drone.fastlanetravis_ciproofofconcept -b app/build/outputs/apk/app-release.apk

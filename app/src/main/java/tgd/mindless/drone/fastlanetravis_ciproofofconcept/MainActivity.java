da6efc64780a3d5fe353e96e043ce0f898353cdb
tdillon <tdillon@users.noreply.github.com>
2016-07-19 20:43:31 -0400

    trying conditional tag based builds/deploys

--- app/src/main/java/tgd/mindless/drone/fastlanetravis_ciproofofconcept/MainActivity.java
+++ app/src/main/java/tgd/mindless/drone/fastlanetravis_ciproofofconcept/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("Tim is the best!");
     }
 }
